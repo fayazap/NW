40ea7426dc720f3b90d07c4be977a93a0bf9d3b7
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-06-12 20:58:16 +0530

    did lsr

--- internal/lsr/lsr.c
+++ internal/lsr/lsr.c
@@ -3,8 +3,8 @@
 #include <limits.h>
 int main()
 {
-    int h, adj[50][50], d;
-    printf("Enter the no of hops: \n");
+    int h, adj[50][50];
+    printf("Enter the number of hops: \n");
     scanf("%d", &h);
     int distance[h], visited[h];
     for (int i = 0; i < h; i++)
@@ -12,6 +12,7 @@ int main()
         distance[i] = INT_MAX;
         visited[i] = 0;
     }
+    distance[0] = 0;
     printf("Enter the adjacency matrix: \n");
     for (int i = 0; i < h; i++)
     {
@@ -20,32 +21,28 @@ int main()
             scanf("%d", &adj[i][j]);
         }
     }
-    distance[0] = 0;
     for (int i = 0; i < h - 1; i++)
     {
         int min = INT_MAX, min_index;
         for (int j = 0; j < h; j++)
         {
-            if (visited[j] == 0 && distance[j] < min)
+
+            if (!visited[j] && distance[j] < min)
             {
                 min = distance[j];
                 min_index = j;
             }
             visited[min_index] = 1;
         }
-
-        for (int d = 0; d < h; d++)
-        {
-            if (!visited[d] && adj[min_index][d] && distance[min_index] != INT_MAX && distance[min_index] + adj[min_index][d] < distance[d])
-            {
-                distance[d] = distance[min_index] + adj[min_index][d];
+        for(int d=0;d<h;d++){
+            if(!visited[d] && adj[min_index][d] && distance[min_index] !=INT_MAX && distance[min_index]+adj[min_index][d]<distance[d]){
+                distance[d]=adj[min_index][d]+distance[min_index];
             }
         }
-        printf("\n");
     }
-    printf("Vertex   Distance from Source: \n");
-    for (int i = 0; i < h; i++)
-    {
-        printf("%d\t%d\t\n", i, distance[i]);
+    printf("Result: \n");
+    printf("node\tDistance\n");
+    for(int i=0;i<h;i++){
+        printf("%d\t%d\n",i,distance[i]);
     }
 }
