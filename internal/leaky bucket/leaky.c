9f2dc3a8b2d7c9f93a5b8a1f2ec424066e67a551
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-06-14 20:41:07 +0530

    leaky done

--- internal/leaky bucket/leaky.c
+++ internal/leaky bucket/leaky.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-    int bucket_size,occupied=0,n,out_rate,in;
-    printf("Enter the number of packets: \n");
-    scanf("%d",&n);
+
+void main(){
+    int bucket_size,occupied=0,n,output_rate;
     printf("Enter the bucket size: \n");
     scanf("%d",&bucket_size);
+    printf("Enter the number of inputs: \n");
+    scanf("%d",&n);
     printf("Enter the output rate: \n");
-    scanf("%d",&out_rate);
+    scanf("%d",&output_rate);
     while(n!=0){
-        printf("Enter the size of input: \n");
+        printf("Enter the input size: \n");
+        int in;
         scanf("%d",&in);
         if(in<=(bucket_size-occupied)){
             occupied+=in;
             printf("Status: %d out of %d\n",occupied,bucket_size);
-        }else{
-            printf("Dropping %d\n",in-(bucket_size-occupied));
+        }
+        else {
+            printf("Discarded: %d\n",in-(bucket_size-occupied));
             occupied=bucket_size;
             printf("Status: %d out of %d\n",occupied,bucket_size);
         }
-        printf("Status: %d out of %d\n",occupied,bucket_size);
+        occupied-=output_rate;
         n--;
-        occupied-=out_rate;
+        printf("Status after removal: %d out of %d\n",occupied,bucket_size);
     }
 }
