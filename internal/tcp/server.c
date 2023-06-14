a5dd2ca5ca85d0fda6b9ba34e54903a0d1c69010
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-06-14 22:13:32 +0530

    tcp

--- internal/tcp/server.c
+++ internal/tcp/server.c
@@ -1,48 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
-
+#include <arpa/inet.h>
 int main()
 {
-    int server, client;
-    char buffer[1024];
+    int client, server;
     struct sockaddr_in servAddr;
     struct sockaddr_storage store;
     socklen_t addrSize;
-
+    char buffer[1024];
     server = socket(AF_INET, SOCK_STREAM, 0);
     servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(2255);
+    servAddr.sin_port = htons(6565);
     servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr));
     if (server < 0)
     {
-        printf("Socket creation failed..\n");
+        printf("Socket creation error..\n");
     }
     else
     {
         printf("Socket created successfully..\n");
     }
-
-    bind(server, (struct sockaddr *)&servAddr, sizeof(servAddr));
     if (listen(server, 5) == 0)
     {
         printf("Listening..\n");
     }
     else
     {
-        printf("Something went wrong...\n");
+        printf("Error..\n");
     }
     while (1)
     {
         client = accept(server, (struct sockaddr *)&store, &addrSize);
         recv(client, buffer, 1024, 0);
-        printf("From client: %s\n", buffer);
-        printf("Enter the content to be send to client: \n");
+        printf("Content from client: %s\n", buffer);
+        printf("Enter the content to send to client: \n");
         scanf("%s", buffer);
-        send(client, buffer, sizeof(buffer),0);
+        send(client, buffer, sizeof(buffer), 0);
     }
 }
