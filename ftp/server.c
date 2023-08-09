434b66bbe253d7ad6ff4f47bd06ab5b95fc9948f
amaldevcd <mailtoamaldevcd@gmail.com>
2023-08-09 09:11:41 +0530

    ftp updated

--- ftp/server.c
+++ ftp/server.c
@@ -1,61 +1,52 @@
 #include <stdio.h>
-#include <arpa/inet.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-#define SERV_TCP_PORT 5035
-#define MAX 60
-
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, clength;
-     struct sockaddr_in serv_addr, cli_addr;
-     char str[MAX];
-
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(SERV_TCP_PORT);
-
-     printf("\nBinded");
-     bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-     printf("\nListening...");
-     listen(sockfd, 5);
-
-     clength = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clength);
-     close(sockfd);
-
-     read(newsockfd, str, MAX);
-     str[MAX - 1] = '\0'; // Ensure string termination
-
-     printf("\nClient message\nFile Name: %s\n", str);
-
-     FILE *f1 = fopen(str, "r");
-     if (f1 == NULL)
-     {
-          perror("File not found");
-          exit(EXIT_FAILURE);
-     }
-
-     char buff[4096];
-     size_t n;
-     while ((n = fread(buff, sizeof(char), sizeof(buff), f1)) > 0)
-     {
-          if (write(newsockfd, buff, n) == -1)
-          {
-               perror("write");
-               exit(EXIT_FAILURE);
-          }
-     }
-
-     fclose(f1);
-     printf("\nFile Transferred\n");
-
-     return 0;
+int main(){
+FILE *fp;
+int sd, newsd, ser, n, a, cli, pid, bd, port, clilen;
+char name[100], fileread[100], fname[100], ch, file[100], rcv[100];
+struct sockaddr_in servaddr, cliaddr;
+printf("Enter the port address\n");
+scanf("%d", &port);
+sd = socket(AF_INET, SOCK_STREAM, 0);
+if (sd < 0)
+printf("Cant create\n");
+else
+printf("Socket is created\n");
+servaddr.sin_family = AF_INET;
+servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+servaddr.sin_port = htons(port);
+a = sizeof(servaddr);
+bd = bind(sd, (struct sockaddr *)&servaddr, a);
+if (bd < 0)
+printf("Cant bind\n");
+else
+printf("Binded\n");
+listen(sd, 5);
+clilen = sizeof(cliaddr);
+newsd = accept(sd, (struct sockaddr *)&cliaddr, &clilen);
+if (newsd < 0){
+printf("Cant accept\n");
+}
+else
+printf("Accepted\n");
+n = recv(newsd, rcv, 100, 0);
+rcv[n] = '\0';
+fp = fopen(rcv, "r");
+if (fp == NULL){
+send(newsd, "error", 5, 0);
+close(newsd);
+}else {
+while (fgets(fileread, sizeof(fileread), fp)) {
+if (send(newsd, fileread, sizeof(fileread), 0) < 0) {
+printf("Can’t send file contents\n");
+}
+sleep(1);
+}
+send(newsd, "completed", 100, 0);
+return 0;
+}
 }
