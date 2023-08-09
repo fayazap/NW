48630fb608b71046cbe5469661898aa58c8f3c9d
fayazap <84265684+fayazap@users.noreply.github.com>
2023-08-09 14:04:58 +0530

    Update leaky.c

--- leaky bucket/leaky.c
+++ leaky bucket/leaky.c
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 
 void main(){
-    int bucket_size,occupied=0,n,output_rate;
+    int bucket_size,occupied=0,n,output_rate, int remaining=0;
     printf("Enter the bucket size: \n");
     scanf("%d",&bucket_size);
+    remaining = bucket_size();
     printf("Enter the number of inputs: \n");
     scanf("%d",&n);
     printf("Enter the output rate: \n");
@@ -13,16 +14,18 @@ void main(){
         printf("Enter the input size: \n");
         int in;
         scanf("%d",&in);
-        if(in<=(bucket_size-occupied)){
+        remaining = bucket_size-occupied;
+        if(in<=(remaining)){
             occupied+=in;
             printf("Status: %d out of %d\n",occupied,bucket_size);
         }
         else {
-            printf("Discarded: %d\n",in-(bucket_size-occupied));
+            printf("Discarded: %d\n",in-(remaining));
             occupied=bucket_size;
             printf("Status: %d out of %d\n",occupied,bucket_size);
         }
         occupied-=output_rate;
+        remaining += output_rate;
         n--;
         printf("Status after removal: %d out of %d\n",occupied,bucket_size);
     }
